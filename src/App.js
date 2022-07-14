2f9dd72737bd8cae44eca10289857096497c79bc
Dastan Alymkulov <alymkulov58@gmail.com>
2022-07-14 18:42:43 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './components/Header/Header'
-
+import PostList from './components/PostList/PostList'
+import './style/App.css'
 const App = () => {
 	return (
-		<>
+		<div>
 			<Header />
-		</>
+			<div className='wrapper'>
+				<PostList />
+			</div>
+		</div>
 	)
 }
 export default App
